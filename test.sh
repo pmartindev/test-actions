5b90041999f25bb13d4fdf0f598ff4bcc68cf129
pmartindev <pmartindev@github.com>
2022-03-11 15:41:34 -0600

    Added new trim

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 CLONE_DIR=.
 GITHUB_ENV="github_env$(date +%s)"
 if [[ -f "$CLONE_DIR/action.yml" ]]; then
-    echo "::set-output name=using::$(cat "$CLONE_DIR/action.yml" | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ')"
+    echo "::set-output name=using::$(cat "$CLONE_DIR/action.yml" | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ' | tr -d "\'")"
 elif [[ -f "$CLONE_DIR/action.yaml" ]]; then
     echo "DOCKER_IMAGE=$(cat $CLONE_DIR/action.yaml | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ')" >> $GITHUB_ENV
 fi
