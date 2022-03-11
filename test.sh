8b4b848787c607a31338f4e2f6fb67a2d3fbe502
pmartindev <pmartindev@github.com>
2022-03-11 15:33:00 -0600

    Added new needs statements.

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ GITHUB_ENV="github_env$(date +%s)"
 if [[ -f "$CLONE_DIR/action.yml" ]]; then
     echo "::set-output name=using::$(cat "$CLONE_DIR/action.yml" | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ')"
 elif [[ -f "$CLONE_DIR/action.yaml" ]]; then
-  echo "DOCKER_IMAGE=$(cat $CLONE_DIR/action.yaml | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ')" >> $GITHUB_ENV
+    echo "DOCKER_IMAGE=$(cat $CLONE_DIR/action.yaml | grep -o 'using:.*' | cut -d ':' -f 2 | tr -d ' ')" >> $GITHUB_ENV
 fi
 cat $GITHUB_ENV
